58248d396e29623adecfb901b35b4fa087eb4101
upalinandy <nandyupali@gmail.com>
2016-11-04 13:48:41 +0530

    removing the leading \ from batch file to get the relative path

--- specrunexefromteamcity.bat
+++ specrunexefromteamcity.bat
@@ -1,3 +1,3 @@
-"\packages\SpecRun.Runner.1.5.2\tools\SpecRun.exe" run Default.srprofile /baseFolder:"\SpecFrame\bin\Release" /log:specrun.log
+"packages\SpecRun.Runner.1.5.2\tools\SpecRun.exe" run Default.srprofile /baseFolder:"SpecFrame\bin\Release" /log:specrun.log
 
 
